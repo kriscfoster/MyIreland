fe2d89f89487becedb702ce66816ce86c8c3722f
“kriscfoster”! <krisfoster@live.ie>
2018-01-06 10:31:56 +0000

    fix

--- js/Ireland.js
+++ js/Ireland.js
@@ -4,8 +4,6 @@ scene = new THREE.Scene();
 renderer = new THREE.WebGLRenderer({ antialias: false, alpha: true });
 camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 controls = new THREE.OrbitControls(camera);
-//controls.target.y -= 0.8;
-//controls.target.z = 2;
 require("./helper.js");
 require("./mapSetup.js");
 const raycaster = new THREE.Raycaster();
@@ -88,7 +86,7 @@ globalObject = {
           imageDiv = document.createElement("div");
           imageDiv.setAttribute('class', "sightsListItemImageDiv");
           name = document.createTextNode(entry.name);
-          stars = document.createTextNode(entry.rating + "\u{272D}".repeat(Math.round(entry.rating)));
+          stars = entry.rating > 0 ? document.createTextNode(entry.rating + "\u{272D}".repeat(Math.round(entry.rating))) : document.createTextNode("");
           image = document.createElement("img");
           image.setAttribute('class', "sightsListItemImage");
           image.src = entry.imageUrl;
@@ -248,7 +246,6 @@ function render() {
   }
 
   renderer.render(scene, camera);
-    console.log(camera.position);
 };
 
 window.addEventListener("mousemove", onMouseMove);
